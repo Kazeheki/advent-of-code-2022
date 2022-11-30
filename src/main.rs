d7a26591f65cf5d0370adbedb1b2a843c835a944
Kazeheki <11597609+Kazeheki@users.noreply.github.com>
2022-11-30 11:40:10 +0100

    feat: add arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use clap::Parser;
+
+#[derive(clap::Parser)]
+struct Args {
+    #[arg(short, long)]
+    day: u8,
+
+    #[arg(short, long, value_enum, default_value_t = Mode::ALL)]
+    mode: Mode,
+}
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum Mode {
+    ALL,
+    PROBLEM1,
+    PROBLEM2,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = Args::parse();
+
+    let result = match arguments.mode {
+        Mode::ALL => {
+            let result1 = run_problem1(arguments.day);
+            let result2 = run_problem2(arguments.day);
+            result1.and(result2)
+        }
+        Mode::PROBLEM1 => run_problem1(arguments.day),
+        Mode::PROBLEM2 => run_problem2(arguments.day),
+    };
+
+    if result.is_err() {
+        eprintln!("Error: {}", result.err().unwrap());
+    } else {
+        println!("[[SUCCESS]]");
+    }
+}
+
+fn run_problem1(day: u8) -> Result<(), String> {
+    if !day_exists(day) {
+        return Err("Day has no input yet".to_string());
+    }
+    Ok(())
+}
+
+fn run_problem2(day: u8) -> Result<(), String> {
+    if !day_exists(day) {
+        return Err("Day has no input yet".to_string());
+    }
+    Ok(())
+}
+
+fn day_exists(day: u8) -> bool {
+    let path = format!("input/{:02}.txt", day);
+    std::path::Path::new(&path).exists()
 }
