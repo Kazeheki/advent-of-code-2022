2d03b21b88e3adb611f2033ffe86f75bb756a71b
Kazeheki <11597609+Kazeheki@users.noreply.github.com>
2022-12-01 08:35:07 +0100

    refactor: read-buffer on delegation

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,7 +1,9 @@
-pub fn problem1() -> Result<String, String> {
+use std::{fs::File, io::BufReader};
+
+pub fn problem1(buffer: BufReader<File>) -> Result<String, String> {
     Err("Not implemented".to_string())
 }
 
-pub fn problem2() -> Result<String, String> {
+pub fn problem2(buffer: BufReader<File>) -> Result<String, String> {
     Err("Not implemented".to_string())
 }
