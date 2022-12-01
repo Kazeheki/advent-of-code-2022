a29ce93c1e3f1dd5f94fb943758f57fa92d7b10d
Kazeheki <11597609+Kazeheki@users.noreply.github.com>
2022-12-01 09:53:11 +0100

    refactor: mark unused var
    so that the compiler doesn't complain for the not yet implemented
    days.

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,9 +1,9 @@
 use std::{fs::File, io::BufReader};
 
-pub fn problem1(buffer: BufReader<File>) -> Result<String, String> {
+pub fn problem1(_buffer: BufReader<File>) -> Result<String, String> {
     Err("Not implemented".to_string())
 }
 
-pub fn problem2(buffer: BufReader<File>) -> Result<String, String> {
+pub fn problem2(_buffer: BufReader<File>) -> Result<String, String> {
     Err("Not implemented".to_string())
 }
