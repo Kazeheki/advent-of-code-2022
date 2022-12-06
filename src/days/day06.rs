ab8d3eb8d1dec8f8979a4958147eb5998ebc25ff
Kazeheki <11597609+Kazeheki@users.noreply.github.com>
2022-12-06 07:28:00 +0100

    feat: day06 - part1

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,11 +1,40 @@
-use std::{fs::File, io::BufReader};
+use std::{
+    collections::HashSet,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-pub fn problem1(_buffer: BufReader<File>) -> Result<String, String> {
-    Err("Not implemented".to_string())
+pub fn problem1(buffer: BufReader<File>) -> Result<String, String> {
+    let file = buffer
+        .lines()
+        .into_iter()
+        .map(|x| x.unwrap().to_string() + "\n")
+        .collect::<String>();
+
+    let result = index_first_4_unique(&file).expect("No index found") + 4;
+
+    Ok(result.to_string())
 }
 
 pub fn problem2(_buffer: BufReader<File>) -> Result<String, String> {
     Err("Not implemented".to_string())
+}
+
+fn index_first_4_unique(input: &String) -> Option<usize> {
+    let chars: Vec<char> = input.chars().collect();
+
+    let mut index_of_first_4_unique: Option<usize> = None;
+    for (pos, _) in chars.iter().enumerate() {
+        let (_, interest) = chars.split_at(pos);
+        let (head, _) = interest.split_at(4);
+        let unique: HashSet<char> = HashSet::from_iter(head.iter().cloned());
+        if unique.len() == 4 {
+            index_of_first_4_unique = Some(pos);
+            break;
+        }
+    }
+
+    index_of_first_4_unique
 }
 
 #[cfg(test)]
