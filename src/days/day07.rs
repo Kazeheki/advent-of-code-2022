c1654061c8fbd411605ce9ba7b983a5e7b190c99
Kazeheki <11597609+Kazeheki@users.noreply.github.com>
2022-12-10 17:21:25 +0100

    feat: day07 - part1

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,7 +1,55 @@
-use std::{fs::File, io::BufReader};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-pub fn problem1(_buffer: BufReader<File>) -> Result<String, String> {
-    Err("Not implemented".to_string())
+pub fn problem1(buffer: BufReader<File>) -> Result<String, String> {
+    // started with tree and failed..
+    // this solution is influenced by ThePrimeagen noting in his stream solving AoC 07, that the input is alread DFS.
+    // I feel so stupid right now...
+
+    const MAX_SIZE: u32 = 100_000;
+    const CD_COMAND: &str = "$ cd ";
+
+    // (dir-name, dir-size) dir-name only for debugging
+    let mut stack: Vec<(String, u32)> = vec![("/".to_string(), 0)];
+    let mut sum: u32 = 0;
+
+    for line_result in buffer.lines() {
+        let mut line = line_result.unwrap();
+
+        // "cd /" only happens at beginning in example and my input.
+        if line == "$ cd /" || line == "$ ls" {
+            continue;
+        }
+
+        if line.starts_with(CD_COMAND) {
+            let dir = line.split_off(CD_COMAND.len());
+
+            if dir == ".." {
+                // pop off and add to parent.
+                let (_, size) = stack.pop().unwrap();
+
+                if size <= MAX_SIZE {
+                    // fill the sum of all dirs under limit.
+                    sum += size;
+                }
+                stack.last_mut().unwrap().1 += size;
+            } else {
+                // add new dir to stack.
+                stack.push((dir, 0));
+            }
+        }
+
+        let (size, _) = line.split_once(" ").unwrap();
+
+        if let Ok(size) = size.parse::<u32>() {
+            stack.last_mut().unwrap().1 += size;
+        }
+        // otherwise it would be information about dir.
+    }
+
+    Ok(sum.to_string())
 }
 
 pub fn problem2(_buffer: BufReader<File>) -> Result<String, String> {
